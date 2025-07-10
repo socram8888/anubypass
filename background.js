e5381ac069279d1b4cac4facfae74526c70f69b0
Marcos Del Sol Vives <marcos@orca.pet>
2025-07-11 01:43:47 +0200

    Introduce known list of instances for faster access

--- background.js
+++ background.js
@@ -1,12 +1,89 @@
 
-// First rule ID for session rules.
-const RULE_OFFSET = 1000;
+// Offset for known rules
+const KNOWN_HOST_RULE_OFFSET = 100;
 
-// Constant was introduced in Chrome 120, so add a fallback for older versions.
-const MAX_RULE_COUNT = chrome.declarativeNetRequest.MAX_NUMBER_OF_SESSION_RULES ?? 1000;
+// Known hosts using the protection
+const KNOWN_HOSTS = [
+	// STARTS KNOWN HOSTS
+	'archives.lib.duke.edu',
+	'bugs.scummvm.org',
+	'bugs.winehq.org',
+	'builder.sourceware.org',
+	'canine.tools',
+	'catgirl.click',
+	'cfaarchive.org',
+	'clew.se',
+	'code.hackerspace.pl',
+	'codeberg.org',
+	'coinhoards.org',
+	'dev.sanctum.geek.nz',
+	'ebird.org',
+	'extensions.typo3.org',
+	'fabulous.systems',
+	'find.library.duke.edu',
+	'forum.freecad.org',
+	'forums.scummvm.org',
+	'gcc.gnu.org',
+	'git.aya.so',
+	'git.devuan.org',
+	'git.enlightenment.org',
+	'git.kernel.org',
+	'git.lupancham.net',
+	'gitea.com',
+	'gitlab.freedesktop.org',
+	'gitlab.gnome.org',
+	'gitlab.postmarketos.org',
+	'hebis.de',
+	'hofstede.io',
+	'hosted.weblate.org',
+	'hydra.nixos.org',
+	'indiemag.fr',
+	'jaredallard.dev',
+	'karla.hds.hebis.de',
+	'linktaco.com',
+	'lore.kernel.org',
+	'mozillazine.org',
+	'nicholas.duke.edu',
+	'openwrt.org',
+	'patchwork.sourceware.org',
+	'pluralpedia.org',
+	'policytoolbox.iiep.unesco.org',
+	'reactos.org',
+	'reddit.nerdvpn.de',
+	'repositorio.ufrn.br',
+	'repository.duke.edu',
+	'rpmfusion.org',
+	'scioly.org',
+	'source.puri.sm',
+	'sourceware.org',
+	'squirreljme.cc',
+	'superlove.sayitditto.net',
+	'svnweb.freebsd.org',
+	'trac.ffmpeg.org',
+	'tufind.hds.hebis.de',
+	'tumfatig.net',
+	'ubmr.hds.hebis.de',
+	'wiki.archlinux.org',
+	'wiki.freecad.org',
+	'wiki.freepascal.org',
+	'wiki.scummvm.org',
+	'xeiaso.net',
+	// ENDS KNOWN HOSTS
+];
+
+// Hosts where enabling the protection would break the site
+const DISABLED_HOSTS = new Set([
+	'canine.tools',
+]);
+
+// First rule ID for session rules.
+const AUTO_RULE_OFFSET = 200;
+
+// Max number of automatic session rules
+const AUTO_RULE_LIMIT = 1000;
 
 // Next rule ID to insert
-let ruleCounter = 0;
+let autoRuleCounter = 0;
 
 // Set with all hosts that have rules.
 const modifiedHosts = new Set();
@@ -96,37 +173,14 @@ function buildFakeUA() {
 }
 
 /**
- * Callback for webRequest onBeforeRequest event.
- *
- * It checks if there is a rule for the current hosts, an adds a new one if not.
- *
- * @param details current web request details.
+ * Builds a new rule for the given host.
+ * @param host host to create a rule for.
+ * @param id rule id.
+ * @returns a new rule object.
  */
-const beforeRequestCallback = async (details) => {
-	const host = new URL(details.url).host;
-	if (modifiedHosts.has(host)) {
-		return;
-	}
-
-	console.log(`Adding rule #${ruleCounter} for ${host}`);
-
-	/*
-	 * Important - we have to add it to the list BEFORE issuing the asynchronous
-	 * updateSessionRules, else there are concurrency issues.
-	 */
-	if (modifiedHostsList.length < MAX_RULE_COUNT) {
-		// If we're not at the limit yet, just add a new one
-		modifiedHosts.add(host);
-		modifiedHostsList.push(host);
-	} else {
-		// Else replace oldest host
-		modifiedHosts.delete(modifiedHostsList[ruleCounter]);
-		modifiedHosts.add(host);
-		modifiedHostsList[ruleCounter] = host;
-	}
-
-	const rule = {
-		id: RULE_OFFSET + ruleCounter,
+function buildRuleForHost(host, id) {
+	return {
+		id,
 		priority: 1,
 		action: {
 			type: 'modifyHeaders',
@@ -147,8 +201,41 @@ const beforeRequestCallback = async (details) => {
 			 */
 			resourceTypes: ALL_RESOURCE_TYPES
 		}
-	};
-	ruleCounter = (ruleCounter + 1) % MAX_RULE_COUNT;
+	}
+}
+
+/**
+ * Callback for webRequest onBeforeRequest event.
+ *
+ * It checks if there is a rule for the current hosts, an adds a new one if not.
+ *
+ * @param details current web request details.
+ */
+const beforeRequestCallback = async (details) => {
+	const host = new URL(details.url).host;
+	if (modifiedHosts.has(host) || DISABLED_HOSTS.has(host)) {
+		return;
+	}
+
+	console.log(`Adding automatic #${autoRuleCounter} for ${host}`);
+
+	const rule = buildRuleForHost(host, AUTO_RULE_OFFSET + autoRuleCounter);
+	autoRuleCounter = (autoRuleCounter + 1) % AUTO_RULE_LIMIT;
+
+	/*
+	 * Important - we have to add it to the list BEFORE issuing the asynchronous
+	 * updateSessionRules, else there are concurrency issues.
+	 */
+	if (modifiedHostsList.length < AUTO_RULE_LIMIT) {
+		// If we're not at the limit yet, just add a new one
+		modifiedHosts.add(host);
+		modifiedHostsList.push(host);
+	} else {
+		// Else replace oldest host
+		modifiedHosts.delete(modifiedHostsList[autoRuleCounter]);
+		modifiedHosts.add(host);
+		modifiedHostsList[autoRuleCounter] = host;
+	}
 
 	// Nothing happens if we attempt to remove a rule that doesn't exist, so we'll do it always
 	await chrome.declarativeNetRequest.updateSessionRules({
@@ -186,12 +273,22 @@ const extensionInstalledCallback = async () => {
 	console.log('Purging old rules');
 
 	// Remove currently set-up session rules, as they persist between extension reloads.
-	const curRules = await chrome.declarativeNetRequest.getSessionRules();
-	if (curRules.length) {
-		await chrome.declarativeNetRequest.updateSessionRules({
-			removeRuleIds: curRules.map(x => x.id),
-		});
+	const oldRules = (await chrome.declarativeNetRequest.getSessionRules()).map(x => x.id);
+
+	// Directly add rules for known hosts
+	const newKnownRules = [];
+	for (const host of KNOWN_HOSTS) {
+		if (!DISABLED_HOSTS.has(host)) {
+			console.log(`Adding known #${newKnownRules.length} for ${host}`);
+			modifiedHosts.add(host);
+			newKnownRules.push(buildRuleForHost(host, KNOWN_HOST_RULE_OFFSET + newKnownRules.length));
+		}
 	}
+
+	await chrome.declarativeNetRequest.updateSessionRules({
+		removeRuleIds: oldRules,
+		addRules: newKnownRules,
+	});
 };
 
 // Install extension installed/updated callback
